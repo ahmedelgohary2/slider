92f02fb966b350036f8e1fb83112a98c21258e57
ahmedelgohary2 <69724895+ahmedelgohary2@users.noreply.github.com>
2020-09-17 06:45:30 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('#satayer').addEventListener('click', function(
+){console.log("hello");});
+var name = for (var i = 0; i < 200; i++) {
+  console.log(i);
+};
+console.log(name);
